19debd324ebcd4a53abb591fe1a3fa4f2a7c250b
lu <lu@leeuwenhoek.mbs.adelaide.edu.au>
2016-05-19 16:19:49 +0930

    add brackets to array

--- overlap.sh
+++ overlap.sh
@@ -7,13 +7,13 @@ arr=('Blood cancer' 'Blood normal' 'Liver cancer' 'Lung normal' 'Bresat normal' 
 arr1=('active_pro' 'weak_pro' 'strong_enhan' 'weak_enhan' 'repressed' 'insulator')
 arr2=('5UTR' 'CDS_exon' 'CDS_intron' '3UTR' 'Intergenic')
 
-for i in "$arr[@]"
+for i in "${arr[@]}"
 do
 
-    for func in "$arr2[@]"
+    for func in "${arr2[@]}"
     do
 
-	for features in "$arr3[@]"
+	for features in "${arr3[@]}"
 	do
 	    
 	    echo $i $func $features
